575020cbdcf4406185a554c384ce17a4db87dd6c
yidie-ling <57084794+yidie-ling@users.noreply.github.com>
2020-01-15 21:49:41 -0800

    added Javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -7,3 +7,13 @@ function initializePage() {
             $('.jumbotron h1').text("Javascript is connected");
             });
 }
+
+$("a.thumbnail").click(projectClick);
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+}
